61d9534d04e19fbe4bfd5af98fc31ac99426c796
Pardis <154790380+pardis-mdd@users.noreply.github.com>
2024-08-28 13:09:39 +0330

    Update Cwe.js

--- src/Cwe.js
+++ src/Cwe.js
@@ -1,49 +1,53 @@
-import { useEffect, useState } from 'react';
-import cweData from './cwe.json'; 
+import { useEffect, useState } from "react";
+import cweData from "./cwe.json";
 
 const Cwe = (props) => {
-    const [cwe, setCwe] = useState(null);  
-    const [cweFound, setCweFound] = useState(false);
+  const [cwe, setCwe] = useState([]);
+  const [cweFound, setCweFound] = useState(false);
 
-    const searchPrimary = props.rawSelectedPath[props.rawSelectedPath.length - 1];
-    const searchFallback = props.rawSelectedPath[props.rawSelectedPath.length - 2];
-    
-    const searchpathPrimary = searchPrimary.replace(/ /g, "_").toLowerCase(); 
-    const searchpathFallback = searchFallback ? searchFallback.replace(/ /g, "_").toLowerCase() : null;
+  const searchPrimary = props.rawSelectedPath[props.rawSelectedPath.length - 1];
+  const searchFallback =
+    props.rawSelectedPath[props.rawSelectedPath.length - 2];
 
-    useEffect(() => {
-      const findCweById = (data, targetId) => {
-        for (let item of data) {
-          if (item.id.toLowerCase() === targetId) {
-            setCwe(item.cwe);
-            setCweFound(true);
-            return; 
-          }
-          if (item.children) {
-            findCweById(item.children, targetId);
-          }
+  const searchpathPrimary = searchPrimary.replace(/ /g, "_").toLowerCase();
+  const searchpathFallback = searchFallback
+    ? searchFallback.replace(/ /g, "_").toLowerCase()
+    : null;
+
+  useEffect(() => {
+    const findCweById = (data, targetId) => {
+      for (let item of data) {
+        if (item.id.toLowerCase() === targetId) {
+          setCwe(item.cwe);
+          setCweFound(true);
+          return;
         }
-      };
-
-      setCwe(null);
-      setCweFound(false);
-
-     
-      findCweById(cweData.content, searchpathPrimary);
-
-      if (!cweFound && searchpathFallback) {
-        findCweById(cweData.content, searchpathFallback);
+        if (item.children) {
+          findCweById(item.children, targetId);
+        }
       }
-    }, [searchpathPrimary, searchpathFallback, cweFound]);
-    return (
-      <div>  
-        {cweFound && cwe ? ( 
-          <h4>{cwe}</h4>
-        ) : (
-          <h3>No CWE found for {searchPrimary} or {searchFallback}</h3>
-        )}
-      </div>
-    );
+    };
+
+    setCwe([]);
+    setCweFound(false);
+    findCweById(cweData.content, searchpathPrimary);
+
+    if (!cweFound && searchpathFallback) {
+      findCweById(cweData.content, searchpathFallback);
+    }
+  }, [searchpathPrimary, searchpathFallback]);
+
+  return (
+    <div>
+      {cweFound && cwe ? (
+        cwe.map((code, index) => <h4 key={index}>{code}</h4>)
+      ) : (
+        <h3>
+          No CWE found for {searchPrimary} or {searchFallback}
+        </h3>
+      )}
+    </div>
+  );
 };
 
 export default Cwe;
